54f3bd99809174483faaafc28525af8e44f08e28
RaghuveerSingh2005 <raghuveersinghshekhawat2005@gmail.com>
2026-05-27 03:15:35 +0530

    sidebar fixed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,7 +7,7 @@ import {
 
 export default function Sidebar() {
   return (
-    <aside className="w-24 min-h-screen bg-[#111827] border-r border-[#374151] p-4 sticky top-0">
+    <aside className="w-20 md:w-64 min-h-screen bg-[#111827] border-r border-[#374151] p-4 sticky top-0">
 
       <h1 className="pixel-title text-xs md:text-lg text-cyan-400 mb-10">
         RAGHUVEER.DEV
@@ -15,26 +15,30 @@ export default function Sidebar() {
 
       <nav className="space-y-4">
 
-        <NavItem icon={<FaHome />} text="Home" />
-        <NavItem icon={<FaCode />} text="Skills" />
-        <NavItem icon={<FaFolder />} text="Projects" />
-        <NavItem icon={<FaTerminal />} text="Terminal" />
+        <NavItem icon={<FaHome />} text="Home" link="#home" />
+
+        <NavItem icon={<FaCode />} text="Skills" link="#skills" />
+
+        <NavItem icon={<FaFolder />} text="Projects" link="#projects" />
+
+        <NavItem icon={<FaTerminal />} text="Terminal" link="#terminal" />
 
       </nav>
     </aside>
   );
 }
 
-function NavItem({ icon, text }) {
+function NavItem({ icon, text, link }) {
   return (
-    <div className="flex items-center gap-3 border border-[#374151] p-3 hover:border-cyan-400 cursor-pointer transition">
-
+    <a
+      href={link}
+      className="flex items-center gap-3 border border-[#374151] p-3 hover:border-cyan-400 cursor-pointer transition"
+    >
       {icon}
 
       <span className="hidden md:block">
         {text}
       </span>
-
-    </div>
+    </a>
   );
 }
