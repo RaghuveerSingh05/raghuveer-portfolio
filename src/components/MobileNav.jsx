5c78efe72d75dc828bebb64a912627ba20633ec4
RaghuveerSingh2005 <raghuveersinghshekhawat2005@gmail.com>
2026-05-27 15:09:28 +0530

    updated MobileNav

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -1,5 +1,3 @@
-// src/components/MobileNav.jsx
-
 import {
   FaHome,
   FaCode,
@@ -10,22 +8,56 @@ import {
 
 export default function MobileNav() {
   return (
-    <div className="md:hidden fixed top-0 left-0 right-0 z-50 bg-[#E7DFC9] border-b-4 border-[#3A312B] p-4">
+    <div className="md:hidden fixed bottom-0 left-0 right-0 z-50 bg-[#E7DFC9] border-t-4 border-[#3A312B] px-2 py-3">
 
-      <div className="flex justify-around text-[#6D28D9]">
+      <div className="flex justify-between items-center text-[#6D28D9]">
 
-        <a href="#home"><FaHome /></a>
+        {/* HOME */}
+        <a
+          href="#home"
+          className="flex flex-col items-center text-xs active:scale-95 transition"
+        >
+          <FaHome className="text-lg" />
+          Home
+        </a>
 
-        <a href="#skills"><FaCode /></a>
+        {/* SKILLS */}
+        <a
+          href="#skills"
+          className="flex flex-col items-center text-xs active:scale-95 transition"
+        >
+          <FaCode className="text-lg" />
+          Skills
+        </a>
 
-        <a href="#projects"><FaFolder /></a>
+        {/* PROJECTS */}
+        <a
+          href="#projects"
+          className="flex flex-col items-center text-xs active:scale-95 transition"
+        >
+          <FaFolder className="text-lg" />
+          Work
+        </a>
 
-        
+        {/* TERMINAL (IMPORTANT FIX) */}
+        <a
+          href="#terminal"
+          className="flex flex-col items-center text-xs active:scale-95 transition"
+        >
+          <FaTerminal className="text-lg" />
+          Terminal
+        </a>
 
-        <a href="#contact"><FaEnvelope /></a>
+        {/* CONTACT */}
+        <a
+          href="#contact"
+          className="flex flex-col items-center text-xs active:scale-95 transition"
+        >
+          <FaEnvelope className="text-lg" />
+          Contact
+        </a>
 
       </div>
-
     </div>
   );
 }
